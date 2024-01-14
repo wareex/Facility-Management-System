b86a945d52e4b976d022602d5714979ee38f6a69
Busari Wareez <wareezbusari@gmail.com>
2024-01-14 09:46:30 +0100

    me

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ include 'header.php'
 
     <!-- Main Footer -->
     <footer class="main-footer">
-      <strong>Copyright &copy; 2023 <a href="#">FACILITY MANAGEMENT SYSTEM. By Mardiya Bello</a>.</strong>
+      <strong>Copyright &copy; 2023 <a href="#">FACILITY MANAGEMENT SYSTEM. By Wareez Busari</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
         <b><?php echo $_SESSION['system']['name']
